6d665b77cef34331d0a0cbc8b93dd213362a4a5e
Javier Cervantes Villanueva <javier.cervantes.villanueva@cern.ch>
2019-05-28 13:44:06 +0200

    Detect operating system before setting the environment

--- init.sh
+++ init.sh
@@ -1,7 +1,17 @@
+#!/bin/bash
+#
+# This script prepares the environment to use a local installation of Heepy
+#
+# Assumes the following configuration:
+# - compiler: gcc62
+# - buildmode: opt (Release)
+# - os: based on the local host
 
+# Detect local host OS
+TOOLSPATH=/cvmfs/fcc.cern.ch/sw/0.8.3/tools/
+OS=`python $TOOLSPATH/hsf_get_platform.py --get os`
 
-source /cvmfs/fcc.cern.ch/sw/views/releases/externals/94.2.0/x86_64-centos7-gcc62-opt/setup.sh
-
+source /cvmfs/fcc.cern.ch/sw/views/releases/externals/94.2.0/x86_64-${OS}-gcc62-opt/setup.sh
 
 export HEPPY=$PWD
 export PATH=$HEPPY/bin:$PATH
